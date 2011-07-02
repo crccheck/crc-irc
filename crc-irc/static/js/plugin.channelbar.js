24926da9d5e10b1315eb82f9aaa5450f4e421a7b
crccheck <c@crccheck.com>
2011-07-02 02:28:04 -0500

    make channels resizable
    fix height calculated wrong

--- crc-irc/static/js/plugin.channelbar.js
+++ crc-irc/static/js/plugin.channelbar.js
@@ -1,12 +1,13 @@
 
 (function(){
-  MODULE_KEY = 'channelbar';
-  MODULE_CANVAS = document.getElementById('pane-west');
+  var MODULE_KEY = 'channelbar';
+  var MODULE_CANVAS = document.getElementById('pane-west');
 
   var $base = $('<section class="ui-channelbar"></section>').appendTo($(MODULE_CANVAS)),
       $main = $('#pane-main');
 
-  $(CANVAS).bind('create', function(e, chan){
+
+  function addChanToBar(chan){
     var button = $('<div class="button">'+chan.channel+'</div>');
     button.click(function(e){
       var visible = chan.$elem.is(':visible');
@@ -20,5 +21,24 @@
 
     });
     $base.append(button);
+  }
+
+
+  function makeChanResizable($chan){
+    $chan.resizable({
+      handles: 's',
+      minHeight: 35,
+      stop: function(e, ui){
+        var children = ui.element.children();
+        var elems = ui.element.children('aside, ol');
+        elems.height(ui.size.height - children.filter('header').outerHeight());
+      }
+    });
+  }
+
+
+  $(CANVAS).bind('create', function(e, chan){
+    addChanToBar(chan);
+    makeChanResizable(chan.$elem);
   });
 })();
