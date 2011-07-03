01a4a2afb967f7afd5d2b4fabc24dfd2dce9cb5d
crccheck <c@crccheck.com>
2011-07-03 12:38:32 -0500

    remember channel show/hide status
    remember channel show/hide status

--- crc-irc/static/js/plugin.channelbar.js
+++ crc-irc/static/js/plugin.channelbar.js
@@ -3,21 +3,37 @@
   var MODULE_KEY = 'channelbar';
   var MODULE_CANVAS = document.getElementById('pane-west');
 
+  var state = deserialize(MODULE_KEY);
+
+  function saveState(){
+    serialize(MODULE_KEY, state);
+  }
+
   var $base = $('<section class="ui-channelbar"></section>').appendTo($(MODULE_CANVAS)),
       $main = $('#pane-main'),
       offset;
 
 
   function addChanToBar(chan){
+    chan._channelbar = {};
     var button = $('<div class="button">'+chan.channel+'</div>');
+    chan._channelbar.button = button;
+    chan._channelbar.hide = function(){
+      chan.$elem.hide();
+      button.addClass('closed');
+    };
+    chan._channelbar.show = function(){
+      button.removeClass('closed');
+      chan.$elem.prependTo($main).show();
+    };
     button.click(function(e){
       var visible = chan.$elem.is(':visible');
+      state[chan.channel].visible = !visible;
+      saveState();
       if (visible) {
-        chan.$elem.hide();
-        button.addClass('closed');
+        chan._channelbar.hide();
       } else {
-        button.removeClass('closed');
-        chan.$elem.prependTo($main).show();
+        chan._channelbar.show();
       }
     });
     $base.append(button);
@@ -45,6 +61,12 @@
   $(CANVAS).bind('create', function(e, chan){
     addChanToBar(chan);
     makeChanResizable(chan.$elem);
+    if (!state[chan.channel]) {
+      state[chan.channel] = {};
+    }
+    if (state[chan.channel].visible === false) {
+      chan._channelbar.hide();
+    }
     updateOffset();
   });
 
